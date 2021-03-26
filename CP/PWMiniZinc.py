9f3cff2c58da5aecc24d44c353faa96563808adb
Alessandro Pavesi <pavesialessandro@live.it>
2021-03-26 17:10:33 +0100

    CP modified behaviour for rotation

--- CP/PWMiniZinc.py
+++ CP/PWMiniZinc.py
@@ -22,7 +22,7 @@ def read_txt(path):
     return w, h, n_papers, papers
 
 
-def draw_single_solution(result, fig, fig_rows, i, colors):
+def draw_single_solution_rotation(result, fig, fig_rows, i, colors):
 	# Read the first line which contains the width and the height of the paper roll
 	solution_in_line = str(result).strip().split("\n")
 
@@ -53,22 +53,53 @@ def draw_single_solution(result, fig, fig_rows, i, colors):
 
 	axs.set(adjustable='box', aspect='equal')
 
+def draw_single_solution(result, fig, fig_rows, i, colors):
+	# Read the first line which contains the width and the height of the paper roll
+	solution_in_line = str(result).strip().split("\n")
 
-def print_solution(resultList, solutionCounter=6):
+	width = int(solution_in_line[0].split(" ")[0])
+	height = int(solution_in_line[0].split(" ")[1])
+
+	# Read the second line which contains the number of necessary pieces of paper to cut off
+
+	number_of_pieces = int(solution_in_line[1].strip())
+
+	papers = []
+
+	for line in solution_in_line[2:]:
+		line = line.split()
+		papers.append([int(line[0]),int(line[1]),int(line[2]),int(line[3])])
+
+	print("Width: {}, Height: {}, N: {}, Paper:{}".format(width, height, number_of_pieces, papers))
+	axs=fig.add_subplot(fig_rows, 2, i+1)
+
+	for k,paper in enumerate(papers):
+		sq = Rectangle((paper[2],paper[3]),paper[0],paper[1],fill = True,color=colors[k], alpha=.3 )
+		axs.add_patch(sq)
+
+	axs.set(adjustable='box', aspect='equal')
+
+def print_solution(resultList, solutionCounter=6, user_rotation=False):
     fig_rows = math.ceil(solutionCounter/2)
     fig = plt.figure(figsize=(fig_rows, 2))
     fig.suptitle("Solutions")
     colors = ["#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)]) for k in range(50)]
 
     if type(resultList.solution) is not list:
-        draw_single_solution(str(resultList.solution), fig, fig_rows, 0, colors)
+        if user_rotation:
+            draw_single_solution_rotation(str(resultList.solution), fig, fig_rows, 0, colors)
+        else:
+            draw_single_solution(str(resultList.solution), fig, fig_rows, 0, colors)
         plt.plot()
 
     else:
         for i in range(len(resultList.solution)):
             if i < solutionCounter:
                 r = resultList.solution[i]
-                draw_single_solution(r, fig, fig_rows, i, colors)
+                if user_rotation:
+                    draw_single_solution_rotation(r, fig, fig_rows, i, colors)
+                else:
+                    draw_single_solution(r, fig, fig_rows, i, colors)
                 plt.plot()
 
     plt.show()
@@ -95,16 +126,24 @@ def main():
         instance_choose_name = str(input("\nWrong choice.\nChoose an instance: [without the extension]\n"))  #"08x08"
         instance_choose = instance_choose_name + ".dzn"
 
-    print("Solving...")
+    user_rotation = str(input("\n\nDo yo want to use rotation: [Y/N]\n")).upper()
+    while user_rotation not in ["Y", "N"]:
+        user_rotation = str(input("\nWrong choice.\nDo yo want to use rotation: [Y/N]\n")).upper()
+
+    print("Building the Model...")
     gecode = Solver.lookup("gecode")
 
     model = Model()
-    model.add_file("CP/OptimizationProject.mzn")
+    if user_rotation == "Y":
+        model.add_file("CP/OptimizationProjectRotation.mzn")
+    else:
+        model.add_file("CP/OptimizationProject.mzn")
+
     model.add_file(path_dzn + instance_choose)
 
     instance = Instance(gecode, model)
 
-
+    print("Solving...")
     all_sol = False
     counter_solutions = 10
     if all_sol:
